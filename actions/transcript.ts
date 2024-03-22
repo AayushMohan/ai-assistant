dc351fe53fc92b46f6a8f0a8ef4fee26b44b2c09
AayushMohan <aayushmohan1702@gmail.com>
2024-03-22 23:50:36 +0530

    Refactor Azure credentials check in transcript function

--- actions/transcript.ts
+++ actions/transcript.ts
@@ -5,8 +5,16 @@ import { AzureKeyCredential, ChatRequestMessage, OpenAIClient } from "@azure/ope
 async function transcript(prevState: any, { formData }: { formData: any }) {
  console.log("PREVIOUS STATE", prevState)
 
- if (process.env.AZURE_API_KEY === undefined || process.env.AZURE_ENDPOINT === undefined || process.env.AZURE_DEPLOYMENT_NAME === undefined || process.env.AZURE_DEPLOYMENT_COMPLETIONS_NAME === undefined) {
+ if (process.env.AZURE_API_KEY === undefined ||
+  process.env.AZURE_ENDPOINT === undefined ||
+  process.env.AZURE_DEPLOYMENT_NAME === undefined ||
+  process.env.AZURE_DEPLOYMENT_COMPLETIONS_NAME === undefined
+ ) {
   console.error("Azure credentials not set")
+  return {
+   sender: "",
+   response: "Azure credentials not set",
+  }
  }
 }
 
