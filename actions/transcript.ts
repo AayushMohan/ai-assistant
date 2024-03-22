0602fd4f98cbcc377efc389b396a3d93ab1fef8a
AayushMohan <aayushmohan1702@gmail.com>
2024-03-22 23:49:24 +0530

    Refactor transcript function to include error handling for missing Azure credentials

--- actions/transcript.ts
+++ actions/transcript.ts
@@ -2,6 +2,12 @@
 
 import { AzureKeyCredential, ChatRequestMessage, OpenAIClient } from "@azure/openai"
 
-async function transcript(prevState: any, { formData: FormData }) { }
+async function transcript(prevState: any, { formData }: { formData: any }) {
+ console.log("PREVIOUS STATE", prevState)
+
+ if (process.env.AZURE_API_KEY === undefined || process.env.AZURE_ENDPOINT === undefined || process.env.AZURE_DEPLOYMENT_NAME === undefined || process.env.AZURE_DEPLOYMENT_COMPLETIONS_NAME === undefined) {
+  console.error("Azure credentials not set")
+ }
+}
 
 export default transcript
