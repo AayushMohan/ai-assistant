6948ba727b7e63ed7ad56411cd55f226b5fecd28
AayushMohan <aayushmohan1702@gmail.com>
2024-03-22 15:11:06 +0530

    Add profile picture and settings icon to header

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+import { SettingsIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       {/* Header */}
-      <header>
+      <header className="flex justify-between fixed top-0 text-white w-full p-5">
         <Image
           src="https://avatars.githubusercontent.com/u/66319691?v=4"
           height={50}
           width={50}
           alt="Profile Picture"
-          className="rounded-full"
+          className="rounded-full object-cover"
+        />
+        <SettingsIcon
+          size={40}
+          className="p-2 m-2 rounded-full cursor-pointer bg-gray-200 hover:bg-gray-300 text-black transition-all ease-in-out duration-150"
         />
       </header>
 
